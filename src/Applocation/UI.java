4fa6a75f6e94ecdfe56d561bc0961fa9963af1fd
Ismael Moreira <imcalazans@gmail.com>
2022-11-19 09:30:31 -0300

    Placing pieces on the board

--- src/Applocation/UI.java
+++ src/Applocation/UI.java
@@ -18,6 +18,8 @@ public class UI {
 	private static void printPiece(ChessPiece piece){
 		if(piece == null) {
 			System.out.print("- ");
+		}else {
+			System.out.print(piece);
 		}
 		
 	}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -8,6 +11,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		InitialSetup();
 	}
 	
 	public ChessPiece[][] getPiece(){
@@ -18,6 +22,12 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void InitialSetup() {
+		board.placePiece(new Rook(board, Color.White), new Position(2, 1));
+		board.placePiece(new King(board, Color.Black), new Position(0, 4));
+		board.placePiece(new King(board, Color.White), new Position(7, 4));
 	}
 	
 
